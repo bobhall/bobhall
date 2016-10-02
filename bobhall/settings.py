6e2356936ef7d7d3c4f67f985f2635ef52304fba
Bob Hall <Robert.Hall@vaisala.com>
2016-10-02 16:56:05 -0700

    debug=False in settings

--- bobhall/settings.py
+++ bobhall/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!8g!ci*=ah*jj$*gwu(#=!a#b_-*7*+n&_+b8uyntbv-vqclg*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
